89e68a76233935d4b9789301a8a5029ac4a21c26
Silas Prado <silas.prado98@gmail.com>
2023-05-18 18:23:33 -0300

    add comments

--- Ordenação/insertionSort.c
+++ Ordenação/insertionSort.c
@@ -6,17 +6,14 @@ void insert(int v[], int n) {
         //armazena a 2ªposição do vetor na variável x
         x = v[i];
         
-        //atualiza a variável j para indicar a posição anterior a armazenada para comparar
-        j = i - 1;
-        
-        //realiza um while para empurrar os valores para direira em caso do vetor na posição anterior ser menor que o valor posterior armazenado
-        while(j >= 0 && v[j] > x){
-            v[j+1] = v[j];
+        //realiza um for para empurrar os valores para direira em caso do vetor na posição anterior(j) ser menor que o valor posterior armazenado
+        for(j = i-1; j >= 0 && v[j] > x; j--){
             //atualiza j para indicar posição anterior à correta para armazenar x após o deslocamento 
-            j--;
-            //x é armazenado na posição posterior de j que indicava a posição anterior à correta
-            v[j+1] = x;
+            v[j+1] = v[j];
         }
+        
+        //x é armazenado na posição posterior de j que indicava a posição anterior à correta
+        v[j+1] = x;
     }
 }
 
@@ -36,3 +33,9 @@ int main()
 
     return 0;
 }
+
+// Complexidade de tempo O(n²)
+// É boa em casos em que:
+// Se tenha pequenas quantidades de elementos 
+// Em sequências de entrada quase ordenadas, pois a inserção de um elemento em uma posição quase correta requer um número menor de comparações
+// Quando se tem uma parte da sequência já ordenada, pode ser eficiente para ordenar a parte desornada
