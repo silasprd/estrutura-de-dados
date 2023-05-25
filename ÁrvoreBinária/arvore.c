295a274b94a6da9012d86ded4c7ea0107359d33b
silasprd <silas.prado98@gmail.com>
2023-05-25 14:05:14 -0300

    fix binary tree

--- ÁrvoreBinária/arvore.c
+++ ÁrvoreBinária/arvore.c
@@ -1,59 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct cel {
-    int content;
-    struct cel *left;
-    struct cel *right;
-};
+typedef struct node {
+    int value;
+    struct node *left;
+    struct node *right;
+} Node;
 
-typedef struct cel node;
-
-typedef node *tree;
-
-tree insert(node *r, int value){
-    node *f, *p;
-    if(r == NULL){
-        return r;
+Node* insert(Node *root, int value){
+    
+    Node *f, *p;
+    Node *new = malloc (sizeof(Node));
+    new->value = value;
+    new->left = NULL;
+    new->right = NULL;
+    
+    if(root == NULL){
+        return new;
     }
     
-    f = r;
+    f = root;
     
     while (f != NULL){
+        
         p = f;
-        if(value < f->content){
+        
+        if(f->value > new->value){
             f = f->left;
         } else {
             f = f->right;
         }
+        
     }
     
-    if(p->content > value){
-        p->left->content = value;
+    if(p->value > new->value){
+        p->left = new;
     } else {
-        p->right->content = value;
+        p->right = new;
     }
     
-    return r;
+    return root;
 }
 
-void print(node *r){
-    if(r != NULL){
-        print(r->left);
-        printf("%d", r->content);
-        print(r->right);
+// void print(Node *root){
+//     if(root != NULL){
+//         print(root->left);
+//         printf("%d ", root->value);
+//         print(root->right);
+//     }
+// }
+
+void printTreeStructure(Node* root, int space) {
+    
+    if (root == NULL) {
+        return;
     }
+
+    printTreeStructure(root->right, space + 1);
+
+    for (int i = 0; i < space; i++) {
+        printf("    ");
+    }
+
+    printf("%d\n", root->value);
+
+    printTreeStructure(root->left, space + 1);
+    
 }
 
 int main()
 {
-    printf("Hello World");
-
-    node *new = malloc (sizeof(node));
-    
-    insert(new, 8);
-    insert(new, 5);
-    insert(new, 10);
-    
+    Node *root = NULL;
+    root = insert(root, 20);
+    root = insert(root, 10);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 12);
+    root = insert(root, 14);
+    root = insert(root, 22);
+    printTreeStructure(root, 0);
     return 0;
 }
